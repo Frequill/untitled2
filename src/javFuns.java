fe4e7b971c93107dc7f3332c3dd085d79ff9fc6f
Julius <julius.thomsen99@gmail.com>
2021-09-16 13:49:00 +0200

    I DON'T LIKE Counter-Strike Russians!!

--- src/javFuns.java
+++ src/javFuns.java
@@ -1,6 +1,6 @@
 public class javFuns {
     public static void main(String[] args) {
         System.out.println("Detta skall committas!!");
-        System.out.println("Death to all russians");
+        System.out.println("I DON'T LIKE RUSSIANS IN CS");
     }
 }
